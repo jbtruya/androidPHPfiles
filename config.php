c761c52d9197acd8a5aea0914ba4856b2bd74239
jbtruya <70558120+jbtruya@users.noreply.github.com>
2022-01-24 12:25:24 +0800

    Update config.php

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
     define("DATABASE", 'u157984996_android');
     define("USER", 'u157984996_androiduser');
-    define("PASSWORD", 'Thepotato1!');
+    define("PASSWORD", 'Password');
     define("SERVER", '127.0.0.1:3306');
 
 
